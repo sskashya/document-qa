44e3c2359fb5f24c28b5f011eee7178894b82300
sskashya <156260545+sskashya@users.noreply.github.com>
2024-08-29 16:15:31 +0000

    stuck with validation

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,24 +18,24 @@ else:
 
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
-
+    if client:
     # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+        uploaded_file = st.file_uploader(
+            "Upload a document (.txt or .md)", type=("txt", "md")
     )
 
     # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
+        question = st.text_area(
+            "Now ask a question about the document!",
+            placeholder="Can you give me a short summary?",
+            disabled=not uploaded_file,
     )
-
-    if uploaded_file and question:
+    
+        if uploaded_file and question:
 
         # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
+            document = uploaded_file.read().decode()
+            messages = [
             {
                 "role": "user",
                 "content": f"Here's a document: {document} \n\n---\n\n {question}",
@@ -43,11 +43,17 @@ else:
         ]
 
         # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            stream = client.chat.completions.create(
+            model="gpt-40-mini",
             messages=messages,
             stream=True,
         )
 
         # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+            st.write_stream(stream)
+        else:
+            stream = client.chat.completions.create(
+            model="gpt-40-mini",
+            stream=True,
+        )
+            st.write_stream(stream)
