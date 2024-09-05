48534ed60617d30cd6c976ab2a1e14579d19aecc
sskashya <156260545+sskashya@users.noreply.github.com>
2024-09-05 14:28:12 +0000

    renamed pages in multi_page.py

--- multi_page.py
+++ multi_page.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-create_page = st.Page("create.py", title = "Create entry", icon = ":material/add_circle:")
-delete_page = st.Page("delete.py", title = "Delete entry", icon = ":material/delete:")
+lab1_page = st.Page("Lab 1.py", title = "Lab 1")
+lab2_page = st.Page("Lab 2.py", title = "Lab 2")
 
-pg = st.navigation([create_page, delete_page])
+pg = st.navigation([lab1_page, lab2_page])
 st.set_page_config(page_title="Lab Manager", page_icon=":material/edit:")
 pg.run()
