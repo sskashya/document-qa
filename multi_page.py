b5cf00e745495261c3c5ecf200eea2b1ba031298
sskashya <156260545+sskashya@users.noreply.github.com>
2024-09-05 14:33:52 +0000

    .

--- multi_page.py
+++ multi_page.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-lab1_page = st.Page("Lab 1.py", title = "Lab 1", default = True)
-lab2_page = st.Page("Lab 2.py", title = "Lab 2")
+lab1_page = st.Page("Lab_1.py", title = "Lab 1", default = True)
+lab2_page = st.Page("Lab_2.py", title = "Lab 2")
 
 pg = st.navigation([lab1_page, lab2_page])
 st.set_page_config(page_title="Lab Manager")
