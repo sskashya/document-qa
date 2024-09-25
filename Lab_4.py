7d9ddbbab1869ad4afdcbe8b20d4a19dcbc043b1
sskashya <156260545+sskashya@users.noreply.github.com>
2024-09-25 22:29:40 +0000

    lab4 completed

--- Lab_4.py
+++ Lab_4.py
@@ -9,7 +9,7 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import chromadb 
-chroma_client = chromadb.Client()
+chroma_client = chromadb.PersistentClient()
 
 # Show title and description.
 st.title("MY Lab 4 question answering chatbot with ChromaDB")
@@ -39,92 +39,56 @@ if language_model and adv_model:
 else:
     st.subheader("You are having a conversation with gpt-4o-mini")
 
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = [{'role':'assistant', 'content':'How can I help you?'}]
+if topic == "Select your topic":
+    st.warning("Select a topic from the sidebar to continue")
+else:
+    search = st.button(f"Click here to search for helpful documents related to {topic}")
 
-if 'collection' not in st.session_state:
-    st.session_state['collection'] = chroma_client.get_or_create_collection(name = "Mycollection")
-
-folder_path = "datafiles/"
-texts = []
-for file in os.listdir(folder_path):
-    file_extension = file.split('.')[-1]
-    if file_extension == 'pdf':
-        file_path = os.path.join(folder_path, file)
-        with open(file_path, "rb") as pdf_file:
-            reader = PdfReader(pdf_file)
-            for page_num in range(len(reader.pages)):
-                page = reader.pages[page_num]
-                text = page.extract_text()
-    texts += text
-
-filename = []
-for file in os.listdir(folder_path):
-    filename += file 
-
-def add_to_collection(collection, texts, filename):
-    openai_client = st.session_state.openai_client
-    response = openai_client.embeddings.create(
-        input = (text for text in texts),
-        model = "text-embedding-3-small"
-    )
-    embedding = response.data[0].embedding
-    collection.add(
-        documents = [texts],
-        ids = [filename],
-        embeddings = [embedding]
-    )
-
-def query_from_collection(collection, topic):
-    openai_client = st.session_state.openai_client
-    query_response = openai_client.embeddings.create(
-        input = topic,
-        model = "text-embedding-3-small"
-    )
-    query_embedding = query_response.data[0].embedding
-    results = collection.query(
-        query_embeddings = [query_embedding],
-        n_results = 3
-    )
-    for i in range(len(results['documents'][0])):
-        doc = results['documents'][0][i]
-        doc_id = results['ids'][0][i]
-        st.write(f"The following file syllabus may be helpful: {doc_id}")
-
-'''
-sys_messages = 
-Always ask the user "Do you need more info?" after their first prompt. 
-If the user replies with Yes, then provide more info followed by "Do you need more info?" but,
-if they reply with No, the assistant should reply with "What else can I help you with?" and nothing else. 
-Finally, provide answers so that someone who is 10 years-old will be able to understand.
-
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if prompt := st.chat_input("Type Here"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    message = st.session_state.messages[-5:]
-    message.append({"role": "system", "content": sys_messages})
-
-    if language_model == "OpenAI" and not adv_model:
-        # Generate an answer using the OpenAI API.
-        stream = st.session_state.client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=message,
-            stream=True,
+    def add_to_collection(collection, texts, filename):
+        openai_client = st.session_state.openai_client
+        response = openai_client.embeddings.create(
+            input = (texts),
+            model = "text-embedding-3-small"
         )
-    else:
-        stream = st.session_state.client.chat.completions.create(
-            model="gpt-4o",
-            messages=message,
-            stream=True,
+        embedding = response.data[0].embedding
+        collection.add(
+            documents = [texts],
+            ids = [filename],
+            embeddings = [embedding]
         )
-    with st.chat_message("assistant"):
-        response = st.write_stream(stream)
 
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    '''
+    def query_from_collection(collection, topic):
+        openai_client = st.session_state.openai_client
+        query_response = openai_client.embeddings.create(
+            input = topic,
+            model = "text-embedding-3-small"
+        )
+        query_embedding = query_response.data[0].embedding
+        results = collection.query(
+            query_embeddings = [query_embedding],
+            n_results = 3
+        )
+        for i in range(len(results['documents'][0])):
+            doc = results['documents'][0][i]
+            doc_id = results['ids'][0][i]
+            st.success(f"The following file syllabus may be helpful: {doc_id}")
+
+    if 'Lab4_vectorDB' not in st.session_state:
+        st.session_state['Lab4_vectorDB'] = chroma_client.get_or_create_collection(name = "Mycollection")
+
+    folder_path = "datafiles/"
+    for file in os.listdir(folder_path):
+        file_extension = file.split('.')[-1]
+        if file_extension.lower() == 'pdf':
+            file_path = os.path.join(folder_path, file)
+            texts = ""
+            with open(file_path, "rb") as pdf_file:
+                reader = PdfReader(pdf_file)
+                for page_num in range(len(reader.pages)):
+                    page = reader.pages[page_num]
+                    text = page.extract_text()
+                    texts += text
+            add_to_collection(st.session_state['Lab4_vectorDB'], texts, file)
+
+    if search:
+        query_from_collection(st.session_state['collection'], topic)
