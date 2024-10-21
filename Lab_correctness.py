fd2b401f49b47b3e7c38238e20ab5febe54b8965
sskashya <156260545+sskashya@users.noreply.github.com>
2024-10-21 16:20:22 +0000

    ready to submit solution

--- Lab_correctness.py
+++ Lab_correctness.py
@@ -11,10 +11,6 @@ st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-#openai_api_key = st.text_input("OpenAI API Key", type="password")
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 bespoke_key = st.secrets["BESPOKE_KEY"]
 
@@ -28,7 +24,6 @@ def extract_text_from_pdf(pdf_file):
 
 if 'bespoke_client' not in st.session_state:
     st.session_state.bespoke_client = BespokeLabs(
-        # This is the default and can be omitted
         auth_token=bespoke_key,
     )
 
@@ -38,7 +33,6 @@ if 'openai_client' not in st.session_state:
 uploaded_file = st.file_uploader(
             "Upload a document (.pdf)", type=("pdf")
     )
-    # Ask the user for a question via `st.text_area`.
 question = st.text_area(
 "Now ask a question about the document!",
 placeholder="Can you give me a short summary?",
@@ -46,7 +40,6 @@ disabled=not uploaded_file,
 )
 
 if uploaded_file and question:
-# Process the uploaded file and question.
     document = extract_text_from_pdf(uploaded_file)
     st.session_state.messages = [
     {
